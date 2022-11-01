159fbb9a937481c5ae6d4898b1ba63cf97a2d54d
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-11-01 13:31:07 -0300

    basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ const App: React.FC = () => {
     <div>
       <h1 className="text-3xl font-bold text-center">React Calculator</h1>
       {/* calculator */}
-      <div className="w-80 grid grid-cols-4 grid-rows-6 border-4 border-blue-600 mx-auto">
+      <div className="max-w-xs grid grid-cols-4 grid-rows-7 mx-auto">
         {/* output */}
-        <div className="bg-neutral-800 text-white col-span-4">800</div>
+        <div className="flex flex-col items-end justify-around p-2 bg-neutral-900 col-span-4 shadow-md text-end border-zinc-600 border backdrop-blur-lg">
+          {/* prev operand */}
+          <div className="text-lg text-zinc-400">3 *</div>
+          {/* current operand */}
+          <div className="text-3xl">3</div>
+        </div>
         {/* operators */}
         <button className="col-span-2">AC</button>
         <button>DEL</button>
